33ac8286b0666a365ac3c3719e86c1a348e31caa
alamin@andit <webguru071@gmail.com>
2020-10-10 12:09:34 +0600

    Andgarivara Call To Action Area

---
+++ components/Home/andgarivara-call-to-action-area.js
@@ -0,0 +1,23 @@
+function AndgarivaraCallToActionArea() {
+    return (
+        <section className="andgarivara-call-to-action-area padding-zero">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-12 col-sm-12 col-md-8 col-lg-8">
+                        <div className="call-to-action-content">
+                            <h4>আপনি কি ইতিমধ্যে একটি গাড়ির গর্বিত মালিক হয়েছেন? আপনার গাড়ির অব্যবহারিত সময়ে ভাড়া দিয়ে
+                                লাভবান হউন।</h4>
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-12 col-md-4 col-lg-4">
+                        <div className="col-to-action-button">
+                            <a href="rental-owner.html" className="theme-button">আপনার গাড়ি ভাড়া দিন</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AndgarivaraCallToActionArea
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Home/hero";
 import Footer from "../components/Footer";
+import AndgarivaraCallToActionArea from "../components/Home/andgarivara-call-to-action-area";
 
 function Index() {
     return (
         <div className="main">
             <Navbar/>
             <Hero/>
+            <AndgarivaraCallToActionArea/>
             <Footer/>
         </div>
     )
